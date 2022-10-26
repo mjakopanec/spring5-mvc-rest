67e34506f48e01d07788e55e9e7420bd589be1f1
Monika Jakopanec <mjakopanec7@gmail.com>
2022-10-26 21:41:16 +0200

    Added Service and its implementation. Added tests.

--- src/main/java/guru/springfamework/api/v1/model/CategoryDTO.java
+++ src/main/java/guru/springfamework/api/v1/model/CategoryDTO.java
@@ -1,8 +1,13 @@
 package guru.springfamework.api.v1.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * Created by jt on 9/24/17.
  */
+@Getter
+@Setter
 public class CategoryDTO {
     private Long id;
     private String name;
